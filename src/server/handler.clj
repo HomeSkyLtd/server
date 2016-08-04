1f6a6f54bb4facb7f0ff7fa9bfb9a38e6a934218
Henrod <henrique.93.rodrigues@gmail.com>
2016-08-04 17:34:07 -0300

    Working on

--- src/server/handler.clj
+++ src/server/handler.clj
@@ -1,11 +1,18 @@
 (ns server.handler
   (:require [compojure.core :refer :all]
             [compojure.route :as route]
-            [ring.middleware.defaults :refer [wrap-defaults site-defaults]]))
+            [ring.middleware.defaults :refer [wrap-defaults site-defaults]]
+            [ring.middleware.session :refer :all]
+			[ring.util.response :refer :all]))
+
+(defn handler [{session :session}]
+	(response (str "Hello" (:username session))))
 
 (defroutes app-routes
   (GET "/" [] "Hello World")
   (route/not-found "Not Found"))
 
+;(def app
+;  (wrap-defaults app-routes site-defaults))
 (def app
-  (wrap-defaults app-routes site-defaults))
+	(wrap-session handler))
