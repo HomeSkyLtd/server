e4dfe3a174cd4928940ee709c69b45c917e9ef17
Henrod <henrique.93.rodrigues@gmail.com>
2016-08-08 13:17:23 -0300

    Server with session number and generating cookies.

--- src/server/handler.clj
+++ src/server/handler.clj
@@ -2,17 +2,33 @@
   (:require [compojure.core :refer :all]
             [compojure.route :as route]
             [ring.middleware.defaults :refer [wrap-defaults site-defaults]]
-            [ring.middleware.session :refer :all]
+            [ring.middleware.session :refer [wrap-session]]
 			[ring.util.response :refer :all]))
 
-(defn handler [{session :session}]
-	(response (str "Hello" (:username session))))
+(defn handler [request]
+	(let [count ((request :session {}) :count 0)]
+		{
+			:status 200
+			:headers {"Content-Type" "text/html"}
+			:body (if (zero? count)
+						(str request "</br><h1>Hello, Stranger!</h1>")
+						(str request "</br><h1>Hello again (" count ")"))
+			:session {:count (inc count)}
+		}))
+
+(defn- session? [request]
+	(if (empty? (:session request))
+		{:status 403
+		 :headers {"Content-Type" "text/html"}
+		 :body (str "<h1>FORBIDDEN</h1>" request)}
+		{:status 200
+		 :headers {"Content-Type" "text/html"}
+		 :body (str "<h3>Session on</h3>" request)}))
 
 (defroutes app-routes
-  (GET "/" [] "Hello World")
+  (GET "/" request (handler request))
+  (GET "/check" request (session? request))
   (route/not-found "Not Found"))
 
-;(def app
-;  (wrap-defaults app-routes site-defaults))
-(def app
-	(wrap-session handler))
+(def app 
+	(wrap-session app-routes {:cookie-attrs {:max-age 30}}))
