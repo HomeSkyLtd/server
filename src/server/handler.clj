b86fa7e436125494718d4702aab3bd7d39f3a4b6
Henrod <henrique.93.rodrigues@gmail.com>
2016-08-08 14:16:16 -0300

    Handling post request

--- src/server/handler.clj
+++ src/server/handler.clj
@@ -3,32 +3,43 @@
             [compojure.route :as route]
             [ring.middleware.defaults :refer [wrap-defaults site-defaults]]
             [ring.middleware.session :refer [wrap-session]]
-			[ring.util.response :refer :all]))
+			[ring.util.response :refer :all]
+			[ring.middleware.params :refer [wrap-params]]))
 
-(defn handler [request]
+(defn- build-response [status body]
+	"Build response with status and body"
+	{:status status
+	 :headers {"Content-Type" "text/html"}
+	 :body body})
+
+(defn- handler [request]
 	(let [count ((request :session {}) :count 0)]
-		{
-			:status 200
-			:headers {"Content-Type" "text/html"}
-			:body (if (zero? count)
+		(assoc 
+			(build-response
+				200
+				(if (zero? count)
 						(str request "</br><h1>Hello, Stranger!</h1>")
-						(str request "</br><h1>Hello again (" count ")"))
-			:session {:count (inc count)}
-		}))
+						(str request "</br><h1>Hello again (" count ")")))
+			:session {:count (inc count)})))
 
 (defn- session? [request]
+	"Verify if a request has session number."
 	(if (empty? (:session request))
-		{:status 403
-		 :headers {"Content-Type" "text/html"}
-		 :body (str "<h1>FORBIDDEN</h1>" request)}
-		{:status 200
-		 :headers {"Content-Type" "text/html"}
-		 :body (str "<h3>Session on</h3>" request)}))
+		(build-response 403 (str "<h1>FORBIDDEN</h1>" request))
+		(build-response 200 (str "<h3>Session on</h3>" request))))
+
+(defn- post-params [request]
+	"Return a map with parameters from POST request"
+	(let [name ((:params request) "name") id ((:params request) "id")]
+		{:name name :id id}))
 
 (defroutes app-routes
   (GET "/" request (handler request))
   (GET "/check" request (session? request))
+  (POST "/post" request (str (post-params request)))
   (route/not-found "Not Found"))
 
-(def app 
-	(wrap-session app-routes {:cookie-attrs {:max-age 30}}))
+(def app
+	(-> app-routes
+		(wrap-session {:cookie-attrs {:max-age 30}})
+		(wrap-params)))
