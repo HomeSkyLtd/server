52582e7fe27ee1c6f7fbde5de373d68eaf49b05e
Henrod <henrique.93.rodrigues@gmail.com>
2016-08-09 11:05:28 -0300

    Interacting with dabatase.

--- src/server/db.clj
+++ src/server/db.clj
@@ -6,18 +6,21 @@
 		[com.mongodb MongoOptions ServerAddress DB WriteConcern]
 		[org.bson.types ObjectId]))
 
-(defn db
-	"Get Database by host and port. All parameters are optional."
+(defn init-db
+	"Create Database by host and port. All parameters are optional."
 	([host port db-name]
-		 (mg/get-db (mg/connect {:host host :port port}) db-name))
+		 (def ^:private db (mg/get-db (mg/connect {:host host :port port}) db-name)))
 	([host db-name]
-		 (mg/get-db (mg/connect {:host host}) db-name))
+		 (def ^:private db (mg/get-db (mg/connect {:host host}) db-name)))
 	([db-name]
-		 (mg/get-db (mg/connect) db-name))
+		 (def ^:private db (mg/get-db (mg/connect) db-name)))
 	([]
-		 (mg/get-db (mg/connect) "server-db")))
+		 (def ^:private db (mg/get-db (mg/connect) "server-db"))))
+
+(defn no-db? []
+	"Return true if the db is not initialized yet."
+	(nil? (resolve 'db)))
 
 (defn insert [obj coll-name]
 	"Insert an object (could be a map, a string, etc.) in the collection specified."
-	(let [db (db)]
-		(mc/insert db coll-name (assoc obj :_id (ObjectId.)))))
+	(mc/insert db coll-name (assoc obj :_id (ObjectId.))))
--- src/server/handler.clj
+++ src/server/handler.clj
@@ -8,13 +8,18 @@
 	[ring.util.response :refer :all]
 	[ring.middleware.params :refer [wrap-params]]))
 
-(defn- build-response [status body]
+(defn- build-response
 	"Build response with status and body"
-	{:status status
-	 :headers {"Content-Type" "text/html"}
-	 :body body})
+	([status body] 
+		{:status status
+		 :headers {"Content-Type" "text/html"}
+		 :body body})
+	([status] 
+		{:status status
+		 :headers {"Content-Type" "text/html"}}))
 
 (defn- handler [request]
+	(if (db/no-db?) (db/init-db))
 	(let [count ((request :session {}) :count 0)]
 		(assoc 
 			(build-response
@@ -24,25 +29,25 @@
 						(str request "</br><h1>Hello again (" count ")")))
 			:session {:count (inc count)})))
 
+(defn- handler-db [func & params]
+	(apply func params)
+	(build-response 200))
+
 (defn- session? [request]
 	"Verify if a request has session number."
 	(if (empty? (:session request))
 		(build-response 403 (str "<h1>FORBIDDEN</h1>" request))
 		(build-response 200 (str "<h3>Session on</h3>" request))))
 
-(defn- post-params [request]
-	"Return a map with parameters from POST request"
-	(let [name ((:params request) "name") id ((:params request) "id")]
-		{:name name :id id}))
-
 (defn- insert-in-db [{params :params} coll-name]
 	"Insert params from request in the collection specified."
+	(if (db/no-db?) (db/init-db))
 	(db/insert params coll-name))
 
 (defroutes app-routes
   (GET "/" request (handler request))
   (GET "/check" request (session? request))
-  (POST "/post" request (insert-in-db request "test-table"))
+  (POST "/post" request (handler-db insert-in-db request "testTable"))
   (route/not-found "Not Found"))
 
 (def app
