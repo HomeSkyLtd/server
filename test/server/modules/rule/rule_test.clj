e073afbf8980419964d4e233c7a5d77af97bf601
Henrod <henrique.93.rodrigues@gmail.com>
2016-08-17 18:04:51 -0300

    get rules and get learnt rules

--- test/server/modules/rule/rule_test.clj
+++ test/server/modules/rule/rule_test.clj
@@ -4,8 +4,11 @@
 
 (deftest test-app
   (testing "new node ok"
-    (let [obj {:rules [{:nodeId 1 :commandId 1 :value 20 :clauses [{:lhs "1.1", :operator ">", :rhs "20"}]}]}
-          houseId 1]
+    (let [obj {:rules [
+                  {:nodeId 1 
+                    :commandId 1 
+                    :value 20 
+                    :clauses [{:lhs "1.1", :operator ">", :rhs "20"}]}]} houseId 1]
       (is (= (rule/new-rules obj houseId) {:status 200}))))
 
   (testing "new node without rules"
@@ -13,26 +16,31 @@
       (is (= (rule/new-rules obj houseId) {:status 400 :errorMessage "Rules not defined"}))))
 
   (testing "new node without houseId"
-    (let [obj {:rules [{:nodeId 1 :commandId 1 :value 20 :clauses [{:lhs "1.1", :operator ">", :rhs "20"}]}]}
-          houseId nil]
+    (let [obj {:rules [
+                  {:nodeId 1 
+                    :commandId 1 
+                    :value 20 
+                    :clauses [{:lhs "1.1", :operator ">", :rhs "20"}]}]} houseId nil]
       (is (= (rule/new-rules obj houseId) {:status 400 :errorMessage "houseId not defined"}))))
 
   (testing "new node without nodeId"
     (let [obj {:rules [{:commandId 1 :value 20 :clauses [{:lhs "1.1", :operator ">", :rhs "20"}]}]}
             houseId 1]
-      (is (= (rule/new-rules obj houseId) {:status 400 :errorMessage "Define nodeId, commandId, value and clauses."}))))
+      (is (= (rule/new-rules obj houseId) 
+                {:status 400 :errorMessage "Define nodeId, commandId, value and clauses."}))))
 
-  (testing "new node without commandId"
-    (let [obj {:rules [{:nodeId 1 :value 20 :clauses [{:lhs "1.1", :operator ">", :rhs "20"}]}]}
-            houseId 1]
-      (is (= (rule/new-rules obj houseId) {:status 400 :errorMessage "Define nodeId, commandId, value and clauses."}))))
+  (testing "select from db"
+    (let [houseId 1 obj {:accepted false
+                          :nodeId 1
+                          :commandId 1 
+                          :value 20
+                          :clauses [{:lhs "1.1", :operator ">", :rhs "20"}]}]
+      (is (= (dissoc (first (rule/get-rules houseId)) :_id) obj))))
 
-  (testing "new node without value"
-    (let [obj {:rules [{:nodeId 1 :commandId 1 :clauses [{:lhs "1.1", :operator ">", :rhs "20"}]}]}
-            houseId 1]
-      (is (= (rule/new-rules obj houseId) {:status 400 :errorMessage "Define nodeId, commandId, value and clauses."}))))
-
-  (testing "new node without clauses"
-    (let [obj {:rules [{:nodeId 1 :commandId 1 :value 20}]}
-            houseId 1]
-      (is (= (rule/new-rules obj houseId) {:status 400 :errorMessage "Define nodeId, commandId, value and clauses."})))))
+  (testing "select learnt rules from db"
+    (let [houseId 1 obj {:accepted false 
+                          :nodeId 2 
+                          :commandId 1 
+                          :value 1 
+                          :clauses [{:lhs "2.1", :operator ">", :rhs "0"}]}]
+      (is (= (dissoc (first (rule/get-learnt-rules houseId)) :_id) obj)))))
