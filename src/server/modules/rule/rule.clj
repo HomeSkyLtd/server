7a117c21d193fc7865377bf51f8d348e072292b0
Henrod <henrique.93.rodrigues@gmail.com>
2016-08-17 12:32:37 -0300

    bug fixed, tests working

--- src/server/modules/rule/rule.clj
+++ src/server/modules/rule/rule.clj
@@ -4,16 +4,17 @@
 (def ^:private prefix-name "rules_")
 
 (defn- insert [coll-name obj]
-	(db/insert coll-name obj))
+	(db/insert coll-name obj)
+	{:status 200})
 
 (defn new-rules [obj houseId]
 	"Insert rules into server. These rules that are defined in the App."
 	(if (nil? houseId)
 		{:status 400 :errorMessage "houseId not defined"}
 		(if-let [rules (:rules obj)]
-			(if (and (map #(println "__NEW_DEBUG__" %) rules))
-						;#(and (contains? % :nodeId) (contains? % :commandId) (contains? % :value) (contains? % :clauses)) 
-						;rules))
+			(if (every? true? (map 
+						#(and (contains? % :nodeId) (contains? % :commandId) (contains? % :value) (contains? % :clauses)) 
+						rules))
 					(for [rule rules] (insert (str prefix-name houseId) rule))
 					{:status 400 :errorMessage "Define nodeId, commandId, value and clauses."})
 			{:status 400 :errorMessage "Rules not defined"})))
--- test/server/modules/rule/rule_test.clj
+++ test/server/modules/rule/rule_test.clj
@@ -4,35 +4,35 @@
 
 (deftest test-app
   (testing "new node ok"
-    (let [obj {:rules [{:command {:nodeId 1 :commandId 1 :value 20} :clauses [{:lhs "1.1", :operator ">", :rhs "20"}]}]}
+    (let [obj {:rules [{:nodeId 1 :commandId 1 :value 20 :clauses [{:lhs "1.1", :operator ">", :rhs "20"}]}]}
           houseId 1]
       (is (= (first (rule/new-rules obj houseId)) {:status 200}))))
 
-  (comment (testing "new node without rules"
+  (testing "new node without rules"
     (let [obj nil houseId 1]
       (is (= (rule/new-rules obj houseId) {:status 400 :errorMessage "Rules not defined"}))))
 
   (testing "new node without houseId"
-    (let [obj {:rules [{:command {:nodeId 1 :commandId 1 :value 20} :clauses [{:lhs "1.1", :operator ">", :rhs "20"}]}]}
+    (let [obj {:rules [{:nodeId 1 :commandId 1 :value 20 :clauses [{:lhs "1.1", :operator ">", :rhs "20"}]}]}
           houseId nil]
       (is (= (rule/new-rules obj houseId) {:status 400 :errorMessage "houseId not defined"}))))
 
   (testing "new node without nodeId"
-    (let [obj {:rules [{:command {:commandId 1 :value 20} :clauses [{:lhs "1.1", :operator ">", :rhs "20"}]}]}
+    (let [obj {:rules [{:commandId 1 :value 20 :clauses [{:lhs "1.1", :operator ">", :rhs "20"}]}]}
             houseId 1]
-      (is (= (first (rule/new-rules obj houseId)) {:status 400 :errorMessage "Define nodeId, commandId, value and clauses."}))))
+      (is (= (rule/new-rules obj houseId) {:status 400 :errorMessage "Define nodeId, commandId, value and clauses."}))))
 
   (testing "new node without commandId"
-    (let [obj {:rules [{:command {:nodeId 1 :value 20} :clauses [{:lhs "1.1", :operator ">", :rhs "20"}]}]}
+    (let [obj {:rules [{:nodeId 1 :value 20 :clauses [{:lhs "1.1", :operator ">", :rhs "20"}]}]}
             houseId 1]
-      (is (= (first (rule/new-rules obj houseId)) {:status 400 :errorMessage "Define nodeId, commandId, value and clauses."}))))
+      (is (= (rule/new-rules obj houseId) {:status 400 :errorMessage "Define nodeId, commandId, value and clauses."}))))
 
   (testing "new node without value"
-    (let [obj {:rules [{:command {:nodeId 1 :commandId 1} :clauses [{:lhs "1.1", :operator ">", :rhs "20"}]}]}
+    (let [obj {:rules [{:nodeId 1 :commandId 1 :clauses [{:lhs "1.1", :operator ">", :rhs "20"}]}]}
             houseId 1]
-      (is (= (first (rule/new-rules obj houseId)) {:status 400 :errorMessage "Define nodeId, commandId, value and clauses."}))))
+      (is (= (rule/new-rules obj houseId) {:status 400 :errorMessage "Define nodeId, commandId, value and clauses."}))))
 
   (testing "new node without clauses"
-    (let [obj {:rules [{:command {:nodeId 1 :commandId 1 :value 20}}]}
+    (let [obj {:rules [{:nodeId 1 :commandId 1 :value 20}]}
             houseId 1]
-      (is (= (first (rule/new-rules obj houseId)) {:status 400 :errorMessage "Define nodeId, commandId, value and clauses."}))))))
+      (is (= (rule/new-rules obj houseId) {:status 400 :errorMessage "Define nodeId, commandId, value and clauses."})))))
