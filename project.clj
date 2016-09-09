349d8dd34e7fdaebf5936977aa5f0c07a8f93692
Henrod <henrique.93.rodrigues@gmail.com>
2016-09-09 17:56:20 -0300

    just adding new description

--- project.clj
+++ project.clj
@@ -1,5 +1,7 @@
 (defproject server "1.0.0"
-  :description "Server running machine learning algorithms."
+  :description "Server running machine learning algorithms and communication 
+                protocols between itself, controllers and mobile devices inside 
+                the HomeSky system."
   :url "https://github.com/HomeSkyLtd/server"
   :min-lein-version "2.0.0"
   :dependencies [[org.clojure/clojure "1.7.0"]
@@ -12,13 +14,12 @@
                  [crypto-password "0.2.0"]
                  [try-let "1.1.0"]
                  [org.slf4j/slf4j-nop "1.7.12"]
-                 [clj-http "3.2.0"]
-                 ]
+                 [clj-http "3.2.0"]]
   :main server.handler
   :plugins [[lein-ring "0.9.7"]
             [lein-codox "0.9.7"]]
   :ring {:handler server.handler/app}
-  :codox {:metadata {:doc "I am a variable"}}
+  :codox {:metadata {:doc "I am probably a variable (but also a function)"}}
   :profiles
   {:dev {:dependencies [[javax.servlet/servlet-api "2.5"]
                         [ring/ring-mock "0.3.0"]
